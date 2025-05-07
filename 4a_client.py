d47123f959ebc761b72e1feae22826c5a04ff8be
Ashera2004 <79092592+Ashera2004@users.noreply.github.com>
2025-05-07 18:37:15 +0530

    Add files via upload

---
+++ 4a_client.py
@@ -0,0 +1,16 @@
+#Name: A S Siddarth
+#Reg No. 212224040316
+import socket
+from pythonping import ping
+
+s = socket.socket()
+s.bind(('localhost', 8000)) 
+s.listen(5)
+c, addr = s.accept()
+
+while True:
+    hostname = c.recv(1024).decode()
+    try:
+        c.send(str(ping(hostname, verbose=False)).encode())
+    except KeyError:
+        c.send("Not Found".encode())
---
+++ 4a_server.py
@@ -0,0 +1,9 @@
+#Name: A S Siddarth
+#Reg No. 212224040316
+import socket 
+s=socket.socket() 
+s.connect(('localhost',8000)) 
+while True: 
+    ip=input("Enter the website you want to ping ") 
+    s.send(ip.encode()) 
+    print(s.recv(1024).decode())
